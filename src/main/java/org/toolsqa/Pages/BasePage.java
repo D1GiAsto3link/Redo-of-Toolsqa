b9e13f6b85017c722d4dbcf9052450cf1899a160
D1GiAsto3link <Samad@digilink.africa>
2023-06-12 15:49:21 +0200

    Commit a new

--- src/main/java/org/toolsqa/Pages/BasePage.java
+++ src/main/java/org/toolsqa/Pages/BasePage.java
@@ -1,14 +1,17 @@
 package org.toolsqa.Pages;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 
 public class BasePage {
 
     protected WebDriver driver;
+    protected WebDriverManager Edgedriver;
 
     public BasePage(WebDriver driver) {
 
         this.driver = driver;
+        this.Edgedriver = WebDriverManager.edgedriver();
     }
     // Add common page elements and methods here
 }
